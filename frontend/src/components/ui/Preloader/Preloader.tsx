ac441072b02ac229d9a4c4ac8954180a82818d17
Sergey <supermatrix2001@gmail.com>
2026-04-12 00:37:43 +0300

    feat(frontend): create preloader; update some styles

--- frontend/src/components/ui/Preloader/Preloader.tsx
+++ frontend/src/components/ui/Preloader/Preloader.tsx
@@ -1,8 +1,7 @@
 import styles from './Preloader.module.scss';
 
-// TODO: Обновить стилистически прелоадер
 export const Preloader = () => (
-  <div className={styles.preloader}>
-    <div className={styles.preloader_circle} />
-  </div>
+	<div className={styles.preloader}>
+		<div className={styles.preloader_circle} />
+	</div>
 );
--- frontend/src/components/ui/Text/Text.tsx
+++ frontend/src/components/ui/Text/Text.tsx
@@ -6,9 +6,10 @@ import styles from './Text.module.scss';
 interface TextProps {
 	children: ReactNode;
 	as?: ElementType;
-	size?: 22 | 26 | 40;
+	size?: 14 | 22 | 26 | 40;
 	align?: 'left' | 'center';
 	lowercase?: boolean;
+	invert?: boolean;
 }
 
 export const Text = ({
@@ -17,13 +18,14 @@ export const Text = ({
 	size = 22,
 	align = 'left',
 	lowercase = false,
+	invert = false,
 }: TextProps) => {
-	console.log('Доступные стили:', styles);
 	const className = clsx(
 		styles.text,
 		styles[`size${size}`],
 		styles[`${align}`],
-		{ [styles.lowercase]: lowercase }
+		{ [styles.lowercase]: lowercase },
+		{ [styles.invert]: invert }
 	);
 
 	return <Tag className={className}>{children}</Tag>;
